bfe4c23f27ed05e342482f91158061ea7aee96d3
menayaser80 <menayaser80@gmail.com>
2023-05-13 22:53:44 +0300

    first commit

---
+++ android/app/src/main/java/com/example/beboo/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.beboo;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:beboo/mwareth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+void main()
+{
+  runApp(MaterialApp(home: Mwareeth()));
+}
